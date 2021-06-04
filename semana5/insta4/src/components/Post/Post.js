b7f9534ed8f509335f3d14deaf444c0644d5b25f
daniloac <anthony.danilo@outlook.com.br>
2021-06-04 00:08:54 -0300

    desafios

--- semana5/insta4/src/components/Post/Post.js
+++ semana5/insta4/src/components/Post/Post.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import {IconeComContador} from '../IconeComContador/IconeComContador'
+import { IconeComContador } from '../IconeComContador/IconeComContador'
 
 import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
-import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+import { SecaoComentario } from '../SecaoComentario/SecaoComentario'
+
+import { IconeSemContador } from '../IconeSemContador/IconeSemContador' //Desafio 1
+import iconeMarcacaoBranco from '../../img/flag.svg'         //Desafio 1
+import iconeMarcacaoPreto from '../../img/flag_black.svg'    //Desafio 1
+
+import iconeCompartilharPreto from '../../img/share.svg'
+import { SecaoCompartilhar } from '../SecaoCompartilhar/SecaoCompartilhar' //Desafio 2 e 3
+
 
 const PostContainer = styled.div`
   border: 1px solid gray;
@@ -26,8 +34,14 @@ const PostFooter = styled.div`
   display: flex;
   align-items: center;
   padding: 0 10px;
-  justify-content: space-between;
+  justify-content: flex-start;
+  gap:10px;
 `
+
+const EspacoIconeMarcacao = styled.div`
+  flex-grow: 1;
+`
+
 
 const UserPhoto = styled.img`
   height: 30px;
@@ -45,14 +59,35 @@ class Post extends React.Component {
     curtido: false,
     numeroCurtidas: 0,
     comentando: false,
-    numeroComentarios: 0
+    numeroComentarios: 0,
+    marcacao: false,             // Desafio 1
+    compartilhando: false,
   }
- 
+
+  onClickCompartilhar = () => {
+    this.setState({
+      compartilhando: !this.state.compartilhando
+    })
+  }
+
+  aoCompartilhar = () => {
+    this.setState({
+      compartilhando: false
+    })
+  }
+
+
+  onClickMarcacao = () => {           //Desafio 1
+    this.setState({                   //
+      marcacao: !this.state.marcacao  //
+    })
+  }
+
   onClickCurtida = () => {
     console.log('Curtiu!')
 
     let implementa              // Implementação 3.3
-                                //
+    //
     if (this.state.curtido) {   //
       implementa = -1           //
     } else {                    //
@@ -79,9 +114,17 @@ class Post extends React.Component {
   }
 
   render() {
+    let iconeMarcacao                       //Desafio 1
+
+    if (this.state.marcacao) {              //
+      iconeMarcacao = iconeMarcacaoPreto    //
+    } else {                                //
+      iconeMarcacao = iconeMarcacaoBranco   //
+    }
+
     let iconeCurtida
 
-    if(this.state.curtido) {
+    if (this.state.curtido) {
       iconeCurtida = iconeCoracaoPreto
     } else {
       iconeCurtida = iconeCoracaoBranco
@@ -89,17 +132,23 @@ class Post extends React.Component {
 
     let componenteComentario
 
-    if(this.state.comentando) {
-      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    if (this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario} />
     }
+
+    let componenteCompartilhar //Desafio 2 e 3
+    if (this.state.compartilhando) {
+      componenteCompartilhar = <SecaoCompartilhar aoEnviar={this.aoCompartilhar} />
+    }
+
 
     return <PostContainer>
       <PostHeader>
-        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'} />
         <p>{this.props.nomeUsuario}</p>
       </PostHeader>
 
-      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
+      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'} />
 
       <PostFooter>
         <IconeComContador
@@ -113,8 +162,23 @@ class Post extends React.Component {
           onClickIcone={this.onClickComentario}
           valorContador={this.state.numeroComentarios}
         />
+
+        <IconeSemContador                    //Desafio 2 e 3
+          icone={iconeCompartilharPreto}          //
+          onClickIcone={this.onClickCompartilhar} //
+        />
+
+        <EspacoIconeMarcacao>
+        <IconeSemContador                    //Desafio 1
+          icone={iconeMarcacao}                 //
+          onClickIcone={this.onClickMarcacao}   //
+          />
+        </EspacoIconeMarcacao>
+        
+
       </PostFooter>
       {componenteComentario}
+      {componenteCompartilhar}
     </PostContainer>
   }
 }
