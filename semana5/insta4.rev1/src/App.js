1ab83fb1795f7060994c7955a7dda436061019a7
daniloac <anthony.danilo@outlook.com.br>
2021-06-04 21:49:33 -0300

    s5a20 | complemento exercicio

--- semana5/insta4.rev1/src/App.js
+++ semana5/insta4.rev1/src/App.js
@@ -10,9 +10,48 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state = {
+    postagens: [
+      {nomeUsuario: "paulinha",
+      fotoUsuario: "https://picsum.photos/id/1011/50/50",
+      fotoPost:"https://picsum.photos/200/150?random=1",
+    },
+      {
+        nomeUsuario:"fulana",
+        fotoUsuario:"https://picsum.photos/id/1005/50/50",
+        fotoPost:"https://picsum.photos/200/150?random=2",
+      },
+      {
+        nomeUsuario:"ful",
+        fotoUsuario:"https://picsum.photos/id/1027/50/50",
+        fotoPost:"https://picsum.photos/200/150?random=3",
+      },
+    ]
+
+  }
+
+
   render() {
+
+    const postagem = this.state.postagens.map((post)=>{
+      return (
+        // <MainContainer key={post} >
+        <Post key={post}
+          fotoUsuario={post.fotoUsuario}
+          nomeUsuario={post.nomeUsuario}
+          fotoPost={post.fotoPost}
+          ></Post>
+        // </MainContainer>
+
+      )
+    })
+
     return (
       <MainContainer>
+        {postagem}
+      </MainContainer>
+      /*         
+      <MainContainer>     
         <Post
           nomeUsuario={'paulinha'}
           fotoUsuario={'https://picsum.photos/id/1011/50/50'}
@@ -28,8 +67,8 @@ class App extends React.Component {
           fotoUsuario={'https://picsum.photos/id/1027/50/50'}   //
           fotoPost={'https://picsum.photos/200/150?random=3'}   //
         />
-
       </MainContainer>
+        */
     );
   }
 }
