5d35147b14bd9743e0fd4feb89f86a6257b79a2f
danilo-ac <anthony.danilo@outlook.com.br>
2021-06-15 16:55:11 -0300

    s7a25 | labenuser exercicios

---
+++ semana7/labenusers/app-labenusers/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import axios from 'axios'
+
+export default class App extends React.Component {
+
+  state = {
+    name: "",
+    email: "",
+  }
+
+  onChangeInput = (event) => {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+
+  onClickSalvar = () => {
+
+
+    const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+    const body = {
+      name: this.state.name,
+      email: this.state.email
+    }
+    const header = { headers: { Authorization: 'danilo-chagas-molina' } }
+
+    axios.post(url, body, header)
+      .then(() => { alert("Cadastro realizado com sucesso!") })
+      .catch((erro) => {alert(`Erro ao cadastrar. Tente novamente.\nMensagem do erro: "${erro.response.data.message}"`)})
+
+    this.setState({name:"", email:""})
+  }
+
+  render() {
+
+    return (
+      <div>
+        <input
+          id={"name"}
+          placeholder={"Nome"}
+          value={this.state.name}
+          onChange={this.onChangeInput} />
+        <input
+          id={"email"}
+          placeholder={"Email"}
+          value={this.state.email}
+          onChange={this.onChangeInput} />
+        <button
+          onClick={this.onClickSalvar}
+        >Salvar</button>
+
+      </div>
+    )
+  }
+}
