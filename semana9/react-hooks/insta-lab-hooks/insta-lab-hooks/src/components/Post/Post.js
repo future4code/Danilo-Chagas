8e3f668ffaead5b41738af8bc00d57283d76b865
danilo-ac <anthony.danilo@outlook.com.br>
2021-06-28 17:06:44 -0300

    :arrow_up: atualizaçao exercicio

--- semana9/react-hooks/insta-lab-hooks/insta-lab-hooks/src/components/Post/Post.js
+++ semana9/react-hooks/insta-lab-hooks/insta-lab-hooks/src/components/Post/Post.js
@@ -9,16 +9,46 @@ import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
 
 const Post = (props) => {
-
+const [curtido, setCurtido] = useState(false)
+const [numeroCurtidas, setNumeroCurtidas] = useState(0)
+const [comentando, setComentando] = useState(false)
+const [numeroComentarios, setNumeroComentarios] = useState(0)
+const [comentarios, setComentarios] = useState([])
 
   const onClickCurtida = () => {
+    if(curtido) {
+      setCurtido(!curtido)
+      setNumeroCurtidas(numeroCurtidas -1)
+    } else {
+      setCurtido(!curtido)
+      setNumeroCurtidas(numeroCurtidas +1)
+    }
   };
 
   const onClickComentario = () => {
+    setComentando(!comentando)
   };
 
   const enviarComentario = (comentario) => {
+    const listaDeComentarios = [...comentarios, comentario]
+    setComentarios(listaDeComentarios)
+    setComentando(false)
+    setNumeroComentarios(numeroComentarios + 1)
   }
+
+  const iconeCurtida = curtido ? (iconeCoracaoPreto) : (iconeCoracaoBranco)
+
+  const caixaDeComentario = comentando ? (
+    <SecaoComentario enviarComentario={enviarComentario}/>
+  ) : (
+    comentarios.map(comentario => {
+      return (
+        <CommentContainer>
+          <p>{comentario}</p>
+        </CommentContainer>
+      )
+    })
+  )
 
   return (
     <PostContainer>
@@ -31,9 +61,9 @@ const Post = (props) => {
 
       <PostFooter>
         <IconeComContador
-          // icone={iconeCurtida}
+          icone={iconeCurtida}
           onClickIcone={onClickCurtida}
-          // valorContador={numeroCurtidas}
+          valorContador={numeroCurtidas}
         />
 
         <IconeComContador
@@ -42,7 +72,7 @@ const Post = (props) => {
           // valorContador={numeroComentarios}
         />
       </PostFooter>
-      {/* {caixaDeComentario} */}
+      {caixaDeComentario}
     </PostContainer>
   )
 }
