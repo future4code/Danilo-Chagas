8d4178f5f86d757ff2fc97cdff1db706e7e1eb33
danilo-ac <anthony.danilo@outlook.com.br>
2021-08-06 15:41:31 -0300

    :arrow_up: ajustment on getTarot and added destructuring on received data

--- semana14/personare-tarot/src/App.js
+++ semana14/personare-tarot/src/App.js
@@ -1,7 +1,22 @@
-
+import { useEffect, useState } from "react";
+import { getTarot } from "./services/getTarot";
 function App() {
+
+  const [data, setData] = useState({})
+  const {imagesUrl, imageBackCard, cards} = data
+  const [isLoading, setIsLoading] = useState(false)
+  
+  useEffect(()=>{
+    getTarot(setIsLoading)
+    .then(result => setData(result))
+    
+  },[])
+  
   return (
-    <></>
+    <div>
+      {console.log(imagesUrl)}
+      {isLoading ? '...loading' : 'Tarot'}
+    </div>
 
   );
 }
--- semana14/personare-tarot/src/services/getTarot.js
+++ semana14/personare-tarot/src/services/getTarot.js
@@ -2,11 +2,16 @@ import axios from 'axios'
 
 const BASE_URL = 'https://raw.githubusercontent.com/Personare/front-end-challenge/master/tarot.json'
 
-export const getTarot = async () => {
-    try{
-        const res = await axios.get(BASE_URL)
-        return res
-    } catch (err) {
+export const getTarot = (setIsLoading) => {
+    setIsLoading(true)
+    return axios.get(BASE_URL)
+    .then((res)=>{
+        setIsLoading(false)
+        return res.data
+    })
+    .catch((err)=>{
+        setIsLoading(false)
         alert(`Erro ao carregar cartas.\nTente novamente`)
-    }
+        return false
+    })
 }
