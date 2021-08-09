782770f680f325d801b94d433de80349fb528ba7
danilo-ac <anthony.danilo@outlook.com.br>
2021-08-09 18:50:33 -0300

    Desafio 1 validaçao de inputs

--- semana15/node-package.json/src/exercicios/e2/e2.js
+++ semana15/node-package.json/src/exercicios/e2/e2.js
@@ -1,23 +1,29 @@
-const num1 = process.argv[2] ? Number(process.argv[2]) : false
-const num2 = process.argv[3] ? Number(process.argv[3]) : false
+const operationType = process.argv[2]
+const num1 = process.argv[3] ? Number(process.argv[3]) : false
+const num2 = process.argv[4] ? Number(process.argv[4]) : false
 
-const add = num1 + num2
-const sub = num1 - num2
-const mult = num1 * num2
-const div = num1 / num2
+const opResult = {
+    "add": num1+num2,
+    "sub": num1-num2,
+    "mult": num1*num2,
+    "div": num1/num2,
+}
+
 
 // console.log(`Soma: ${add}\nSubtração: ${sub}\nMultiplicação: ${mult}\nDivisão: ${div}`)
 
 //Desafio 1)
-const inputsValidation = (n1, n2) => {
+
+const inputsValidation = (op, n1, n2) => {
     const validation = {}
-    validation.arg = n1 || n2 ? "São esperados 2 parametros. Está faltando um" : true
-    validation.n1Value = n1 === '' || n1 === false || !isNaN(n1)? 'É esperado um número no 1o parametro' : true
-    validation.n2Value = n2 === '' || n2 === false || !isNaN(n2)? 'É esperado um número no 2o parametro' : true
+    validation.args = op!==false && n1!==false && n2!==false ? true : "São esperados 3 parametros para a operação"
+    validation.operationType = Object.getOwnPropertyNames(opResult).some(item => op===item) ? true : `É esperada uma dessas operações: ${Object.getOwnPropertyNames(opResult)}`
+    validation.n1Value = n1 === '' || n1 === false || isNaN(n1)? 'É esperado um número no 2o parametro' : true
+    validation.n2Value = n2 === '' || n2 === false || isNaN(n2)? 'É esperado um número no 3o parametro' : true
     Object.values(validation).every(item => item === true) ?
-        console.log(`${n1} e ${n2}`)
+        console.log(`Resultado: ${opResult[op]}`)
         :
         (console.log("Verificar os seguintes erros"),
         console.table(Object.values(validation).filter(item => item!==true)))
 }
-inputsValidation(num1, num2)
+inputsValidation(operationType, num1, num2)
