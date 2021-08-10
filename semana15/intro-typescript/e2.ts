c51656043aee3d56adfe1347dcdc4d5350a556b3
danilo-ac <anthony.danilo@outlook.com.br>
2021-08-10 15:36:20 -0300

    e2

--- semana15/intro-typescript/e2.ts
+++ semana15/intro-typescript/e2.ts
@@ -1,17 +1,26 @@
 //2a)
-function obterEstatisticas(numeros) {
+//2a) R.: Entrada: numeros; Saída: estatisticas
+//2b) R.: numerosOrdenados, soma
 
-    const numerosOrdenados = numeros.sort(
+type resultEstatisticas = {
+    maior: number,
+    menor: number,
+    media: number
+}
+
+function obterEstatisticas(numeros: number[]): resultEstatisticas {
+
+    const numerosOrdenados: Array<number> = numeros.sort(
         (a, b) => a - b
     )
 
-    let soma = 0
+    let soma: number = 0
 
     for (let num of numeros) {
         soma += num
     }
 
-    const estatisticas = {
+    const estatisticas: resultEstatisticas = {
         maior: numerosOrdenados[numeros.length - 1],
         menor: numerosOrdenados[0],
         media: soma / numeros.length
@@ -19,4 +28,13 @@ function obterEstatisticas(numeros) {
 
     return estatisticas
 }
-//2a) R. Entrada: numeros; Saída: estatisticas
+//2c)
+type amostra = {
+    numeros: Array<number>
+    obterEstatisticas: (numeros: number[]) => resultEstatisticas
+}
+
+const testeDeAmostragem: amostra = {
+    numeros: [33,27,18],
+    obterEstatisticas,
+}
