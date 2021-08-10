f9e6c1f957ea00c98c9b7ba0a518f563a4aad6eb
danilo-ac <anthony.danilo@outlook.com.br>
2021-08-10 14:57:45 -0300

    exercicios s15a42

---
+++ semana15/intro-typescript/e2.ts
@@ -0,0 +1,22 @@
+//2a)
+function obterEstatisticas(numeros) {
+
+    const numerosOrdenados = numeros.sort(
+        (a, b) => a - b
+    )
+
+    let soma = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
+//2a) R. Entrada: numeros; Saída: estatisticas
