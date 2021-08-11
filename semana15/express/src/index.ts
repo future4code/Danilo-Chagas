cfa8ed3e7743225a006e97a566a24f609ff7d1cb
danilo-ac <anthony.danilo@outlook.com.br>
2021-08-11 16:55:14 -0300

    e3

--- semana15/express/src/index.ts
+++ semana15/express/src/index.ts
@@ -7,14 +7,64 @@ const app: Express = express()
 app.use(express.json())
 app.use(cors())
 
-app.get("/countries/:id", (req: Request, res: Response) => {
-    const result = countries.find(
-        (country)=>{return country.id === Number(req.params.id)}
-    )
+app.get("/countries", (req: Request, res: Response) => {
+    const result = countries.map(item => ({
+        id: item.id,
+        name: item.name
+    }))
 
     res.status(200).send(result)
 })
 
-app.listen(3003, ()=>{
+// app.get("/countries/:id", (req: Request, res: Response) => {
+//     const result = countries.find(
+//         (country) => { return country.id === Number(req.params.id) }
+//     )
+
+//     result ?
+//         res.status(200).send(result) :
+//         res.status(404).send("Not found")
+
+// })
+
+app.get("/countries/search", (req: Request, res: Response) => {
+
+   
+
+
+    // const searchResult = countries.filter(item => {
+    //     return req.query &&
+    //     req.query.name ? item.name.toLowerCase().includes(String(req.query.name).toLowerCase()) : true
+    //     &&
+    //     req.query.capital ? item.capital.toLowerCase().includes(String(req.query.capital).toLowerCase()) : false
+    //     &&
+    //     req.query.continent ? item.continent.toLowerCase().toLocaleString().includes(String(req.query.continent).toLocaleString().toLowerCase()) : false
+    // })
+   
+   
+    const searchResult = countries.filter(item => {
+        
+        let finalResult: Array<boolean> = []
+
+        function resultMount (result: boolean) {
+            finalResult = [...finalResult, result]
+        }
+
+        req.query.name &&
+        resultMount(item.name.toLowerCase().includes(String(req.query.name).toLowerCase()))
+
+        req.query.capital &&
+        resultMount(item.capital.toLowerCase().includes(String(req.query.capital).toLowerCase()))
+
+        req.query.continent &&
+        resultMount(item.continent.toLowerCase().includes(String(req.query.continent).toLowerCase()))
+        
+        return finalResult && item
+    })
+
+    res.status(200).send(searchResult)
+})
+
+app.listen(3003, () => {
     console.log("Servidor iniciado!")
 })
