a47d7c82c3ff939dc101fa035f163ddb07d71f2b
danilo-ac <anthony.danilo@outlook.com.br>
2021-08-12 15:41:52 -0300

    e2

--- semana15/rest/ExercíciosRespostas.md
+++ semana15/rest/ExercíciosRespostas.md
@@ -1,4 +1,6 @@
 1a) requisição com método get
 1b) /users
 
-2a)
+2a) '/users/search' recebe "types" como query para que fique claro ao usuário a forma de busca e para que o express possa identificar a solicitação no path
+2b) utilizado sequencia de condicionais para validar requisição
+
--- semana15/rest/src/index.ts
+++ semana15/rest/src/index.ts
@@ -12,46 +12,46 @@ type User = {
 // Mock simulando um array de usuários no Banco de Dados
 let users: User[] = [
   {
-      id: 1,
-      name: "Alice",
-      email: "alice@email.com",
-      type: "ADMIN",
-      age: 12
+    id: 1,
+    name: "Alice",
+    email: "alice@email.com",
+    type: "ADMIN",
+    age: 12
   },
   {
-      id: 2,
-      name: "Bob",
-      email: "bob@email.com",
-      type: "NORMAL",
-      age: 36
+    id: 2,
+    name: "Bob",
+    email: "bob@email.com",
+    type: "NORMAL",
+    age: 36
   },
   {
-      id: 3,
-      name: "Coragem",
-      email: "coragem@email.com",
-      type: "NORMAL",
-      age: 21
+    id: 3,
+    name: "Coragem",
+    email: "coragem@email.com",
+    type: "NORMAL",
+    age: 21
   },
   {
-      id: 4,
-      name: "Dory",
-      email: "dory@email.com",
-      type: "NORMAL",
-      age: 17
+    id: 4,
+    name: "Dory",
+    email: "dory@email.com",
+    type: "NORMAL",
+    age: 17
   },
   {
-      id: 5,
-      name: "Elsa",
-      email: "elsa@email.com",
-      type: "ADMIN",
-      age: 17
+    id: 5,
+    name: "Elsa",
+    email: "elsa@email.com",
+    type: "ADMIN",
+    age: 17
   },
   {
-      id: 6,
-      name: "Fred",
-      email: "fred@email.com",
-      type: "ADMIN",
-      age: 60
+    id: 6,
+    name: "Fred",
+    email: "fred@email.com",
+    type: "ADMIN",
+    age: 60
   }
 ]
 
@@ -69,6 +69,39 @@ app.listen(3003, () => {
 })
 
 //1
-app.get("/users",(req: Request, res: Response) => {
-  res.status(200).send(users.map(user=>user.name))
+app.get("/users", (req: Request, res: Response) => {
+  res.status(200).send(users.map(user => user.name))
+})
+
+//2
+
+const validTypes = [...new Set(users.map(item => item.type))].sort()
+
+app.get("/users/search", (req: Request, res: Response) => {
+  let erroCode: number = 400
+  const receivedQuery = req.query.types
+ 
+  try {
+
+    if (String(Object.getOwnPropertyNames(req.query)).toLowerCase() !== "types") {
+      erroCode = 400
+      throw new Error("Invalid query or param")
+    } else if (!isNaN(Number(receivedQuery))) {
+      erroCode = 422
+      throw new Error("Only string is valid")
+    } else if (!validTypes.includes(String(receivedQuery).toLocaleUpperCase())) {
+      erroCode = 400
+      throw new Error("Invalid value for Query")
+    } else {
+      const result = users.filter(user => {
+        return user.type === String(receivedQuery).toUpperCase().trim()
+      })
+
+      return res.status(200).send(result)
+    }
+
+  } catch (err) {
+    res.status(erroCode).send({ message: err.message })
+  }
+
 })
