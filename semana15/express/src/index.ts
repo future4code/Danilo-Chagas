0ec46152e9464fbef5c915e038e6d5fa818f268f
danilo-ac <anthony.danilo@outlook.com.br>
2021-08-11 23:41:06 -0300

    e4

--- semana15/express/src/index.ts
+++ semana15/express/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
 import { countries } from './data'
+import { CONTINENTS, country } from './types'
 
 const app: Express = express()
 
@@ -16,16 +17,16 @@ app.get("/countries", (req: Request, res: Response) => {
     res.status(200).send(result)
 })
 
-// app.get("/countries/:id", (req: Request, res: Response) => {
-//     const result = countries.find(
-//         (country) => { return country.id === Number(req.params.id) }
-//     )
+app.get("/countries/:id", (req: Request, res: Response) => {
+    const result = countries.find(
+        (country) => { return country.id === Number(req.params.id) }
+    )
 
-//     result ?
-//         res.status(200).send(result) :
-//         res.status(404).send("Not found")
+    result ?
+        res.status(200).send(result) :
+        res.status(404).send("Not found")
 
-// })
+})
 
 app.get("/countries/search", (req: Request, res: Response) => {
 
@@ -56,12 +57,57 @@ app.get("/countries/search", (req: Request, res: Response) => {
         res.status(200).send(searchResult)
 
     } catch {
-        res.status(400).send({'Bad request by query': inexistentQuery})
+        res.status(400).send({ 'Bad request by query': inexistentQuery })
     }
 
+})
+
+app.post("/countries/:id", (req: Request, res: Response) => {
+
+    const expectedBody: Array<string> = ["name", "capital"]
+    const receivedBody: Array<string> = Object.getOwnPropertyNames(req.body)
+    const editedCountry: country = { id: 0, name: "", capital: "", continent: CONTINENTS.AFRICA }
+    const invalidKeyObject: Array<any> = []
+    const errorType: object = {}
+
+    try {
+
+        const countryID = countries.find((country) => {
+            if (country.id === Number(req.params.id)) {
+                Object.assign(editedCountry, country)
+                return true
+            }
+        })
+
+        if (!countryID) {
+            throw Object.assign(errorType, { "invalidId": Number(req.params.id) })
+        } else if (receivedBody.length === 0) {
+            throw Object.assign(errorType, { "expectedKeys": expectedBody })
+        } else if (
+            !receivedBody.map((item: any) => expectedBody
+                .includes(item) ? true : (invalidKeyObject.push(item), false))
+                .every(item => item)) {
+            throw Object.assign(errorType, { "invalidKey": invalidKeyObject })
+        }
+
+        for (let element in req.body) {
+            if (!isNaN(req.body[element])) {
+                throw Object.assign(errorType, { "invalidValueFor": element })
+            } else if (req.body[element]) {
+                Object.assign(editedCountry, {
+                    [element]: req.body[element]
+                })
+            }
+        }
 
 
+        countries.splice(Number(req.params.id), 1, editedCountry)
 
+        res.status(200).send(editedCountry)
+
+    } catch {
+        res.status(400).send(errorType)
+    }
 
 })
 
