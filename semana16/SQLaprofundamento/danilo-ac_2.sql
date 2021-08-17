27572e71f05eb0ae60fa54eda4b0b535f2cd5fcf
danilo-ac <anthony.danilo@outlook.com.br>
2021-08-17 15:26:13 -0300

    :arrow_up: 2) a b c d

--- semana16/SQLaprofundamento/danilo-ac_2.sql
+++ semana16/SQLaprofundamento/danilo-ac_2.sql
@@ -1,2 +1,33 @@
+/*Importante 2: Lembre-se que para os exercícios de UPDATE e DELETE, o SQL exige que você desative os Safe Updates. Para fazer isso, rode o seguinte comando no seu Workbench: */
+SET SQL_SAFE_UPDATES = 0;
+
 /*1d)Agora,  altere a coluna gender da tabela ACTOR para que ele aceite strings com até 100 caracteres*/
 ALTER TABLE actor CHANGE gender gender VARCHAR(100);
+
+/*2a) Escreva uma query que atualize o nome e a data de nascimento do ator com o id 003*/
+UPDATE actor
+SET name = "Novo Nome", birth_date = "2021/08/17" WHERE id = 003;
+
+/*2b) Escreva uma query que atualize o nome da atriz `Juliana Paes` para `JULIANA PAES`. Então, escreva outra query para voltar ao nome anterior.*/
+UPDATE actor
+SET name = "JULIANA PAES" WHERE name = "Juliana Paes";
+
+UPDATE actor
+SET name = "Juliana Paes" WHERE name = "JULIANA PAES";
+
+/*2c) Escreva uma query que atualize todas as informações do ator com o id `005` */
+UPDATE actor
+SET name = "Novo nome2",
+salary = 1000000,
+birth_date = "2021/08/17",
+gender = "female"
+WHERE id = "005";
+
+/*2d) Escreva uma query em que você tente atualizar um dado da tabela que não existe (com um id inválido, por exemplo). Teste, anote e explique o resultado.*/
+UPDATE actor
+SET nome = "Fulano"
+WHERE id = "080";
+
+UPDATE actor
+SET name = "Fulano"
+WHERE id = "080";
--- semana16/SQLaprofundamento/exercicios.md
+++ semana16/SQLaprofundamento/exercicios.md
@@ -22,11 +22,46 @@
 
 
 ### Exercício 2
-- [] 2a)
-- [] 2b)
-- [] 2c)
-- [] 2d)
+- [x] 2a)
+    ~~~~sql
+    UPDATE actor
+    SET name = "Novo Nome", birth_date = "2021/08/17" WHERE id = 003;
+    ~~~~
+- [x] 2b)
+    ~~~~sql
+    UPDATE actor
+    SET name = "JULIANA PAES" WHERE name = "Juliana Paes";
+    ~~~~
+    
+    ~~~~sql
+    UPDATE actor
+    SET name = "Juliana Paes" WHERE name = "JULIANA PAES";
+    ~~~~
 
+- [x] 2c)
+    ~~~~sql
+    UPDATE actor
+    SET name = "Novo nome2",
+    salary = 1000000,
+    birth_date = "2021/08/17",
+    gender = "female"
+    WHERE id = "005";
+    ~~~~
+- [x] 2d)
+    ~~~~sql
+    UPDATE actor
+    SET nome = "Fulano"
+    WHERE id = "080";
+    
+    UPDATE actor
+    SET name = "Fulano"
+    WHERE id = "080";
+    ~~~~
+    - *Error Code: 1054. Unknown column 'nome' in 'field list'*
+    - *0 row(s) affected Rows matched: 0  Changed: 0  Warnings: 0*
+    - No 1o tentei atualizar a coluna/campo "nome" ao invés de "name" e sucedeu em uma msg de erro informando que a coluna é desconhecida
+    No 2o usei o campo name que consta na tabela, mas com o um id inexistente, nesse caso uma msg de SUCESSO foi retornada, mas informando 0 mundanças.
+ 
 ### Exercício 3
 - [] 3a)
 - [] 3b)
