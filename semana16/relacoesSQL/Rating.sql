9e9b977a04df63fe582abd7323432ba5531292bc
danilo-ac <anthony.danilo@outlook.com.br>
2021-08-19 12:49:59 -0300

    :package: inital commit

---
+++ semana16/relacoesSQL/Rating.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Rating (
+		id VARCHAR(255) PRIMARY KEY,
+    comment TEXT NOT NULL,
+		rate FLOAT NOT NULL,
+    movie_id VARCHAR(255),
+    FOREIGN KEY (movie_id) REFERENCES Movie(id)
+);
