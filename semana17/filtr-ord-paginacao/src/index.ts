0b45621d560f518508d39fad5fd742cb2cdcb876
danilo-ac <anthony.danilo@outlook.com.br>
2021-08-23 17:00:38 -0300

    :arrow_up: E2

--- semana17/filtr-ord-paginacao/src/index.ts
+++ semana17/filtr-ord-paginacao/src/index.ts
@@ -4,20 +4,25 @@ import { AddressInfo } from "net"
 import { getAllUsers } from "./endpoints/getAllUsers"
 import { getUserByName } from "./endpoints/getUserByName"
 import { getUserByType } from "./endpoints/getUserByType"
+import { getUserByNameOrTypeAndOrder } from "./endpoints/getUserByNameOrTypeAndOrder"
 
 export const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-//E0)
+// //E0)
 // app.get("/users", getAllUsers)
 
-//E1a)
+// //E1a)
 // app.get("/users", getUserByName)
 
-//E1b)
-app.get("/users/:type", getUserByType)
+// //E1b)
+// app.get("/users/:type", getUserByType)
+
+// E2)
+app.get("/users", getUserByNameOrTypeAndOrder)
+
 
 const server = app.listen(process.env.PORT || 3003, () => {
    if (server) {
---
+++ semana17/filtr-ord-paginacao/src/queries/selectByUserNameOrTypeAndOrder.ts
@@ -0,0 +1,11 @@
+import { connection } from "../data/connection"
+
+//E2)
+export default async function selectByUserNameOrTypeAndOrder(inputWhere: any, inputOrder: any):Promise<any> {
+    const result = await connection("aula48_exercicio")
+    // .select("*")
+    .where(inputWhere)
+    .orderBy(inputOrder)
+ 
+    return result
+ }
