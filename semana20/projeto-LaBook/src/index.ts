6acbec7afaf533ffb3a4249396f96a99d206af5e
danilo-ac <anthony.danilo@outlook.com.br>
2021-09-14 16:52:40 -0300

    :package: initial setup

---
+++ semana20/projeto-LaBook/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Express } from 'express'
+import cors from 'cors'
+import { AddressInfo } from 'net'
+
+const app: Express = express()
+
+app.use(express.json())
+app.use(cors())
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo
+        console.log("\x1b[42m\x1b[34m%s\x1b[0m", `Server is running in localhost:${address.port}`)
+    } else {
+        console.error("\x1b[41m%s\x1b[0m", `Failure upon starting server`)
+    }
+})
---
+++ semana20/projeto-LaBook/src/services/HashManager.ts
@@ -0,0 +1,18 @@
+import * as bcrypt from "bcryptjs";
+
+export class HashManager {
+
+    async hash(text: string): Promise<string> {
+
+        const cost = 10;
+        const salt = await bcrypt.genSalt(cost);
+        const cypherText = await bcrypt.hash(text, salt);
+
+        return cypherText;
+    }
+
+    async compare(plaintext: string, hash: string): Promise<boolean> {
+        const hashCompare: boolean = await bcrypt.compare(plaintext, hash);
+        return hashCompare;
+    }
+}
