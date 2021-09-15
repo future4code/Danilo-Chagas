3bbea8f8a1bdcc35301d71fa1592ea9c20d943c0
danilo-ac <anthony.danilo@outlook.com.br>
2021-09-14 22:19:46 -0300

    :package: Table migration

--- semana20/projeto-LaBook/src/data/connection.ts
+++ semana20/projeto-LaBook/src/data/connection.ts
@@ -1,16 +1,18 @@
-import { config } from "dotenv";
+import dotenv from 'dotenv'
 import knex from "knex";
 
-config()
+dotenv.config()
 
-export const connection = knex({
+const connection = knex({
     client: "mysql",
     connection: {
         host: process.env.DB_HOST,
-        port: 3306,
         user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA,
+        port: 3306,
         multipleStatements: true
     }
 });
+
+export default connection
