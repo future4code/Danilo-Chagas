b60b6c863d48d4343ed2a476685314301416b23f
danilo-ac <anthony.danilo@outlook.com.br>
2021-09-20 16:02:45 -0300

    :package: 2b

--- semana21/testes-automatizados/tests/performPurchase.test.ts
+++ semana21/testes-automatizados/tests/performPurchase.test.ts
@@ -5,4 +5,8 @@ describe("Teste unitário da função 'performPurchase'", () => {
         const result = performPurchase({name:"Fulano", balance: 500},400)
         expect(result).toEqual({name:"Fulano", balance: 100})
     })
+    test("Teste com um usuário com o saldo igual ao valor de compra", () => {
+        const result = performPurchase({name:"Fulano", balance: 500},500)
+        expect(result).toEqual({name:"Fulano", balance: 0})
+    })
 })
